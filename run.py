4a6b28dfef6b2f39fad658e8d2eaf2405758e3f3
Petr Konupek <petr@konupek.cz>
2015-04-15 16:51:12 +0200

    Uprava cesty ke konfiguraku

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import yaml
 import xlsx2csv
 
-stream = open("/var/data/config.yml", 'r')
+stream = open("/data/config.yml", 'r')
 config = yaml.load(stream);
 path_config = config['path'];
 xlsx2csv.convert_recursive(path_config['input'], 0, path_config['output'], {});
